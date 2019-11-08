b74294aef143d599caa41e0a3338aa33406b48e4
Nic30 <nic30@seznam.cz>
2019-11-08 15:22:33 +0100

    use hwt>=3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='hwtGraph',
       author='Michal Orsak',
       author_email='michal.o.socials@gmail.com',
       install_requires=[
-          'hwt>=2.9',
+          'hwt>=3.0',
       ],
       tests_require=[
           'hwtLib'
